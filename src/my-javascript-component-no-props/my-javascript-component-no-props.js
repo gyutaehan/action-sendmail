d7f474ddbc4378573d95c1e7c993948abdaeea8e
GitHub Action <github-action@users.noreply.github.com>
2020-09-14 13:11:48 +0000

    commit 2645fd7e235ae9a2b1a775ba8894e06a7ab276d6 Author: Han Gyutae <trapkka997@gmail.com> Date:   Mon Sep 14 22:10:58 2020 +0900
        Update publish-docs.yml

--- src/my-javascript-component-no-props/my-javascript-component-no-props.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyJavascriptComponentNoProps = () => {
-    return <div>The name is test!</div>;
-};
-
-export default MyJavascriptComponentNoProps;
--- src/my-javascript-component-no-props/my-javascript-component-no-props.spec.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import MyJavascriptComponentNoProps from 'my-javascript-component-no-props/my-javascript-component-no-props';
-
-export const SpecMyJavascriptComponentNoPropsDefault = () => <MyJavascriptComponentNoProps />;
